c465de94ca9c213f8f1e56b745b983bed4f2ca2b
EnOte <164638951+EnOtese@users.noreply.github.com>
2025-01-05 16:09:44 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+# 1st program
+print(9 ** 0.5 * 5)
+
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+result1 = 2 * 2 + 2
+print(result1)
+result2 = 2 * (2 + 2)
+print(result2)
+print(result1 == result2)
+
+# 4th program
+number_str = '123.456'
+number_float = float(number_str)
+shifted_number = number_float * 10
+int_part = int(shifted_number)
+first_digit_after_decimal = int_part % 10
+print(first_digit_after_decimal)
